274039713d3a6eb51ed95e627332db7d028e2c04
batumzff <batuhanmuzafferoglu@gmail.com>
2024-08-16 20:57:24 +0300

    category detail

--- client/src/Pages/CATEGORY-DETAIL/CategoryDetail.jsx
+++ client/src/Pages/CATEGORY-DETAIL/CategoryDetail.jsx
@@ -4,6 +4,7 @@ import { LiaHeart } from "react-icons/lia";
 import { BsEye } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
 import style from "./CategoryDetail.module.scss"
+import BlogCard from "../../Components/BLOG-CARD/BlogCard";
 
 const CategoryDetail = () => {
   const { categoryDetail, blogs } = useSelector((state) => state.blog);
@@ -18,7 +19,7 @@ const CategoryDetail = () => {
   return (
     <div className={style.main}>
       <div className={style.container}>
-        {detail?.map((item) => (
+        {/* {detail?.map((item) => (
           <div className={style["detail-container"]} key={item._id} onClick={() =>navigate(`/blog-details/${item?._id}`)}>
             <h3>{item.title}</h3>
             <p>{item.content == "" ? "Not a blog found be the first " : item.content}</p>
@@ -28,7 +29,8 @@ const CategoryDetail = () => {
               <span><BsEye />{item.countOfViews?.length}</span>
             </div>
           </div>
-        ))}
+        ))} */}
+        <BlogCard/>
       </div>
     </div>
   );
--- client/src/Router/AppRouter.jsx
+++ client/src/Router/AppRouter.jsx
@@ -9,7 +9,7 @@ const Login = lazy(() => import("../Pages/LOGIN/Login"));
 const Contact = lazy(() => import("../Pages/CONTACT/Contact"));
 const About = lazy(() => import("../Pages/ABOUT/About"));
 const MyProfile = lazy(() => import("../Pages/MY-PROFILE/MyProfile"));
-const NotFound = lazy(() => import("../Pages/NotFound"));
+const NotFound = lazy(() => import("../Pages/NOT-FOUND/NotFound"));
 const BlogDetails = lazy(() => import("../Pages/BLOG-DETAILS/BlogDetails"));
 const NewBlog = lazy(() => import("../Pages/NEW-BLOG/NewBlog"));
 const PrivateRouter = lazy(() => import("./PrivateRouter"));
